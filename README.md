ce0a46f1fc137847706bb978223806cc886af68f
ashleygoez <97846565+ashleygoez@users.noreply.github.com>
2022-02-08 04:33:34 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # random-quotations
+
+A simple quote display node application for DIG4639 Mobile Development at UCF
+
+"You miss 100% of the shots you don't take." - Wayne Gretzky
---
+++ index.js
@@ -0,0 +1,10 @@
+let quotes = [
+    `"You miss 100% of the shots you dont take" - Wayne Gretzy`,
+    `"Fiction reveals truths that reality obscures" - Jessamyn West`,
+    `"There's a world of difference between truths and facts. Facts can obscure the truth." - Maya Angelou`
+]
+
+
+// console.log(Math.floor(random() * 3))
+console.log(quotes[Math.floor(Math.random() * 3)])
+
